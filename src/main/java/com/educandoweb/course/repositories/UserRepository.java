b702f00d25207bb18f30eee0a4ae69ab2bf39c3b
miquiles01 <tatimiquiles@gmail.com>
2025-09-22 10:59:18 -0300

    order, instant and ISO 8601

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.educandoweb.course.entities.User;
 
 
-
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
